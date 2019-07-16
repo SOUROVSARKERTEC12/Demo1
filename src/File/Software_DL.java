47acecd1e79b3214564f62d4e73a57badb93488e
SOUROV <sourov@gmail.com>
2019-07-16 21:51:43 +0600

    Option Show in Screen

---
+++ src/File/Software_DL.java
@@ -0,0 +1,19 @@
+package File;
+
+import java.util.Scanner;
+
+public class Software_DL {
+    public static void main(String[] args) {
+        System.out.println("1.Show all student");
+        System.out.println("2.Show random student");
+        System.out.println("3.Show number of students");
+        System.out.println("4.Add a student");
+        System.out.println("5.Find a student");
+        System.out.println("Enter Option between (1-5):");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int Option = scanner.nextInt();
+
+    }
+}
