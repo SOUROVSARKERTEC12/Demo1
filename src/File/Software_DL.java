a5860d50df187220d3420597c8743034e4b62a41
SOUROV <sourov@gmail.com>
2019-07-16 22:15:16 +0600

    Show all student task completed

--- src/File/Software_DL.java
+++ src/File/Software_DL.java
@@ -1,10 +1,10 @@
 package File;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Software_DL {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("1.Show all student");
         System.out.println("2.Show random student");
         System.out.println("3.Show number of students");
@@ -16,6 +16,20 @@ public class Software_DL {
 
         int Option = scanner.nextInt();
 
+        if(Option == 1) {
+            File file = new File(Constants.TEXT_FILE_NAME);
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(file));
+            }
+            catch(FileNotFoundException e) {
+                System.out.println("Exception"+e.toString());
+            }
 
+            String string;
+            while ((string = bufferedReader.readLine())!=null){
+                System.out.println(string);
+            }
+        }
     }
 }
